01af84b6477df0fc6c00e9ec8b4c62f2a1ed7020
devshagor <shagors333@gmail.com>
2025-12-05 00:24:33 +0600

    Footer and Social links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+import SocialIcons from "@/components/SocialIcons";
+
 export default function Footer() {
     return (
         <footer className="footer">
-            <p>&copy; 2025 NovaStack. All rights reserved.</p>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6 text-center">
+                        <Link href="/" className="site-logo">NovaStack</Link>
+                        <p className="mt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.</p>
+                        <SocialIcons />
+                        
+                        <p className="copyright">&copy; {new Date().getFullYear()} NovaStack Technologies. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
