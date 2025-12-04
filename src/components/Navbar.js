c55342282a7814cfa23c90745eb1c5ba236c11af
devshagor <shagors333@gmail.com>
2025-12-04 23:51:29 +0600

    Header Desktop version update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,32 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="nav">
-            <h2>NovaStack</h2>
-            <div>
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/contact">Contact</Link>
+        <header className="header">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-md-3">
+                        <Link href="/" className="site-logo">NovaStack</Link>
+                    </div>
+                    <div className="col-md-6 text-center">
+                        <nav className="navbar">
+                            <ul>
+                                <li>
+                                    <Link href="/" className="nav-link">Home</Link>
+                                </li>
+                                <li>
+                                    <Link href="/about" className="nav-link">About</Link>
+                                </li>
+                                <li>
+                                    <Link href="/contact" className="nav-link">Contact</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-md-3 text-end">
+                        <Link href="/contact" className="btn btn-primary">Get Quote</Link>
+                    </div>
+                </div>
             </div>
-        </nav>
+        </header>
     );
 }
