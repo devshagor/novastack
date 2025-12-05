338e6062facfd0d05f4dcf70e68faff6aa258511
devshagor <shagors333@gmail.com>
2025-12-05 17:18:10 +0600

    service componentes added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "@/components/Hero";
+import Feature from "@/components/Service";
 
 export default function Home() {
   return (
     <div className="page-content">
       <Hero />
+      <Feature />
     </div>
   );
 }
---
+++ src/components/Service.js
@@ -0,0 +1,99 @@
+import Link from "next/link";
+
+export default function service() {
+    return (
+        <section className="service section-padding">
+            <div className="container">
+                <div className="row text-center justify-content-center">
+                    <div className="col-md-8">
+                        <div className="section-heading">
+                            <h2>Everything You Need to Succeed Online</h2>
+                            <p>From development to marketing, we provide comprehensive digital solutions that drive real results</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-6 col-lg-4">
+                        <div className="service-item">
+                            <div className="service-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                    <path d="M392.8 65.2C375.8 60.3 358.1 70.2 353.2 87.2L225.2 535.2C220.3 552.2 230.2 569.9 247.2 574.8C264.2 579.7 281.9 569.8 286.8 552.8L414.8 104.8C419.7 87.8 409.8 70.1 392.8 65.2zM457.4 201.3C444.9 213.8 444.9 234.1 457.4 246.6L530.8 320L457.4 393.4C444.9 405.9 444.9 426.2 457.4 438.7C469.9 451.2 490.2 451.2 502.7 438.7L598.7 342.7C611.2 330.2 611.2 309.9 598.7 297.4L502.7 201.4C490.2 188.9 469.9 188.9 457.4 201.4zM182.7 201.3C170.2 188.8 149.9 188.8 137.4 201.3L41.4 297.3C28.9 309.8 28.9 330.1 41.4 342.6L137.4 438.6C149.9 451.1 170.2 451.1 182.7 438.6C195.2 426.1 195.2 405.8 182.7 393.3L109.3 320L182.6 246.6C195.1 234.1 195.1 213.8 182.6 201.3z" />
+                                </svg>
+                            </div>
+                            <h3 className="service-title">Web Development</h3>
+                            <p className="service-desc">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using iorem Ipsum is that end.</p>
+                            <Link href="/" className="readmore">Read More</Link>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6 col-lg-4">
+                        <div className="service-item">
+                            <div className="service-icon backend">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                    <path d="M160 96C124.7 96 96 124.7 96 160L96 224C96 259.3 124.7 288 160 288L480 288C515.3 288 544 259.3 544 224L544 160C544 124.7 515.3 96 480 96L160 96zM376 168C389.3 168 400 178.7 400 192C400 205.3 389.3 216 376 216C362.7 216 352 205.3 352 192C352 178.7 362.7 168 376 168zM432 192C432 178.7 442.7 168 456 168C469.3 168 480 178.7 480 192C480 205.3 469.3 216 456 216C442.7 216 432 205.3 432 192zM160 352C124.7 352 96 380.7 96 416L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 416C544 380.7 515.3 352 480 352L160 352zM376 424C389.3 424 400 434.7 400 448C400 461.3 389.3 472 376 472C362.7 472 352 461.3 352 448C352 434.7 362.7 424 376 424zM432 448C432 434.7 442.7 424 456 424C469.3 424 480 434.7 480 448C480 461.3 469.3 472 456 472C442.7 472 432 461.3 432 448z" />
+                                </svg>
+                            </div>
+                            <h3 className="service-title">Backend Development</h3>
+                            <p className="service-desc">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using iorem Ipsum is that end.</p>
+                            <Link href="/" className="readmore">Read More</Link>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6 col-lg-4">
+                        <div className="service-item">
+                            <div className="service-icon cloud">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                    <path d="M112 256C112 167.6 183.6 96 272 96C319.1 96 361.4 116.4 390.7 148.7C401.3 145.6 412.5 144 424 144C490.3 144 544 197.7 544 264C544 277.2 541.9 289.9 537.9 301.8C579.5 322.9 608 366.1 608 416C608 486.7 550.7 544 480 544L176 544C96.5 544 32 479.5 32 400C32 343.2 64.9 294.1 112.7 270.6C112.3 265.8 112 260.9 112 256zM272 144C210.1 144 160 194.1 160 256C160 264.4 160.9 272.6 162.7 280.5C165.4 292.6 158.4 304.8 146.6 308.6C107.9 321 80 357.3 80 400C80 453 123 496 176 496L480 496C524.2 496 560 460.2 560 416C560 378.6 534.3 347.1 499.5 338.4C492 336.5 485.9 331.2 483 324.1C480.1 317 480.9 308.9 485 302.4C492 291.3 496 278.2 496 264.1C496 224.3 463.8 192.1 424 192.1C412.9 192.1 402.5 194.6 393.2 199C382.7 204 370.1 200.7 363.4 191.2C343.1 162.6 309.7 144.1 272.1 144.1z" />
+                                </svg>
+                            </div>
+                            <h3 className="service-title">Cloud Solutions</h3>
+                            <p className="service-desc">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using iorem Ipsum is that end.</p>
+                            <Link href="/" className="readmore">Read More</Link>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6 col-lg-4">
+                        <div className="service-item">
+                            <div className="service-icon cloud">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                    <path d="M112 256C112 167.6 183.6 96 272 96C319.1 96 361.4 116.4 390.7 148.7C401.3 145.6 412.5 144 424 144C490.3 144 544 197.7 544 264C544 277.2 541.9 289.9 537.9 301.8C579.5 322.9 608 366.1 608 416C608 486.7 550.7 544 480 544L176 544C96.5 544 32 479.5 32 400C32 343.2 64.9 294.1 112.7 270.6C112.3 265.8 112 260.9 112 256zM272 144C210.1 144 160 194.1 160 256C160 264.4 160.9 272.6 162.7 280.5C165.4 292.6 158.4 304.8 146.6 308.6C107.9 321 80 357.3 80 400C80 453 123 496 176 496L480 496C524.2 496 560 460.2 560 416C560 378.6 534.3 347.1 499.5 338.4C492 336.5 485.9 331.2 483 324.1C480.1 317 480.9 308.9 485 302.4C492 291.3 496 278.2 496 264.1C496 224.3 463.8 192.1 424 192.1C412.9 192.1 402.5 194.6 393.2 199C382.7 204 370.1 200.7 363.4 191.2C343.1 162.6 309.7 144.1 272.1 144.1z" />
+                                </svg>
+                            </div>
+                            <h3 className="service-title">Cloud Solutions</h3>
+                            <p className="service-desc">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using iorem Ipsum is that end.</p>
+                            <Link href="/" className="readmore">Read More</Link>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6 col-lg-4">
+                        <div className="service-item">
+                            <div className="service-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                    <path d="M112 256C112 167.6 183.6 96 272 96C319.1 96 361.4 116.4 390.7 148.7C401.3 145.6 412.5 144 424 144C490.3 144 544 197.7 544 264C544 277.2 541.9 289.9 537.9 301.8C579.5 322.9 608 366.1 608 416C608 486.7 550.7 544 480 544L176 544C96.5 544 32 479.5 32 400C32 343.2 64.9 294.1 112.7 270.6C112.3 265.8 112 260.9 112 256zM272 144C210.1 144 160 194.1 160 256C160 264.4 160.9 272.6 162.7 280.5C165.4 292.6 158.4 304.8 146.6 308.6C107.9 321 80 357.3 80 400C80 453 123 496 176 496L480 496C524.2 496 560 460.2 560 416C560 378.6 534.3 347.1 499.5 338.4C492 336.5 485.9 331.2 483 324.1C480.1 317 480.9 308.9 485 302.4C492 291.3 496 278.2 496 264.1C496 224.3 463.8 192.1 424 192.1C412.9 192.1 402.5 194.6 393.2 199C382.7 204 370.1 200.7 363.4 191.2C343.1 162.6 309.7 144.1 272.1 144.1z" />
+                                </svg>
+                            </div>
+                            <h3 className="service-title">Cloud Solutions</h3>
+                            <p className="service-desc">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using iorem Ipsum is that end.</p>
+                            <Link href="/" className="readmore">Read More</Link>
+                        </div>
+                    </div>
+                    
+                    <div className="col-md-6 col-lg-4">
+                        <div className="service-item">
+                            <div className="service-icon backend">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                    <path d="M112 256C112 167.6 183.6 96 272 96C319.1 96 361.4 116.4 390.7 148.7C401.3 145.6 412.5 144 424 144C490.3 144 544 197.7 544 264C544 277.2 541.9 289.9 537.9 301.8C579.5 322.9 608 366.1 608 416C608 486.7 550.7 544 480 544L176 544C96.5 544 32 479.5 32 400C32 343.2 64.9 294.1 112.7 270.6C112.3 265.8 112 260.9 112 256zM272 144C210.1 144 160 194.1 160 256C160 264.4 160.9 272.6 162.7 280.5C165.4 292.6 158.4 304.8 146.6 308.6C107.9 321 80 357.3 80 400C80 453 123 496 176 496L480 496C524.2 496 560 460.2 560 416C560 378.6 534.3 347.1 499.5 338.4C492 336.5 485.9 331.2 483 324.1C480.1 317 480.9 308.9 485 302.4C492 291.3 496 278.2 496 264.1C496 224.3 463.8 192.1 424 192.1C412.9 192.1 402.5 194.6 393.2 199C382.7 204 370.1 200.7 363.4 191.2C343.1 162.6 309.7 144.1 272.1 144.1z" />
+                                </svg>
+                            </div>
+                            <h3 className="service-title">Cloud Solutions</h3>
+                            <p className="service-desc">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using iorem Ipsum is that end.</p>
+                            <Link href="/" className="readmore">Read More</Link>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    );
+}
