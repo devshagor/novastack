672028a69ba16368aaf23e5d3e60de7d45876d65
devshagor <shagors333@gmail.com>
2025-12-07 02:19:01 +0600

    navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+    const [open, setOpen] = useState(false);
+
     return (
         <header className="header">
             <div className="container">
                 <div className="row align-items-center">
-                    <div className="col-md-3">
+                    <div className="col-6 col-md-3">
                         <Link href="/" className="site-logo">NovaStack</Link>
                     </div>
-                    <div className="col-md-6 text-center">
+
+                    <div className={`col-md-6 ${open ? "open" : ""}`}>
                         <nav className="navbar">
                             <ul>
                                 <li>
@@ -22,10 +28,17 @@ export default function Navbar() {
                                 </li>
                             </ul>
                         </nav>
+
+                        <div className={`menu-toggle ${open ? "active" : ""}`} onClick={() => setOpen(!open)} aria-label="Toggle Menu">
+                            <span></span>
+                        </div>
+
                     </div>
+
                     <div className="col-md-3 text-end">
-                        <Link href="/contact" className="btn btn-primary">Get Quote</Link>
+                        <Link href="/contact" className="btn btn-cta">Get Quote</Link>
                     </div>
+
                 </div>
             </div>
         </header>
