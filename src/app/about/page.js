2b130589872d5394f74e4a8511c01eb4a4e69342
devshagor <shagors333@gmail.com>
2025-12-06 10:37:50 +0600

    Vision components added

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,9 @@
 import Banner from "@/components/Banner";
 import Story from "@/components/Story";
 import Team from "@/components/Team";
+import Vision from "@/components/Vision";
+import Cta from "@/components/Cta";
+
 
 export const metadata = {
     title: "About Us",
@@ -13,6 +16,8 @@ export default function About() {
             <Banner />
             <Story />
             <Team />
+            <Vision />
+            <Cta />
         </div>
     );
 }
---
+++ src/components/Vision.js
@@ -0,0 +1,23 @@
+
+export default function Vision() {
+    return (
+        <div className="section-padding bg-gray">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <h2>Our Vision</h2>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                    </div>
+                    <div className="col-lg-6">
+                        <h2>Our Mission</h2>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
